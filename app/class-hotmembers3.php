6f94364856f2a985d2bd691b9ecf9e5e329d5935
Marcos Parreiras <marcosfparreiras@gmail.com>
2016-09-16 01:06:32 -0300

    Use on_activate method on activation_hook

--- app/class-hotmembers3.php
+++ app/class-hotmembers3.php
@@ -12,6 +12,15 @@ class Hotmembers3 {
     include self::DIR_PATH . '/controllers/admin-menus-controller.php';
     add_action( 'admin_menu', array( 'Admin_Menus_Controller', 'create_menus') );
   }
+
+  public static function on_activate() {
+    self::create_tables();
+  }
+
+  public static function create_tables() {
+    include self::DIR_PATH . '/models/membership-area.php';
+    Membership_Area::create_table();
+  }
 }
 
 ?>
--- hotmembers3.php
+++ hotmembers3.php
@@ -14,7 +14,6 @@ $hm3_path = dirname(__FILE__) . '/app';
 include_once $hm3_path . '/class-hotmembers3.php';
 new Hotmembers3();
 
-include_once $hm3_path . '/models/membership-area.php';
-register_activation_hook( __FILE__, array( 'Membership_Area', 'create_table' ) );
+register_activation_hook( __FILE__, array( 'Hotmembers3', 'on_activate' ) );
 
 ?>
