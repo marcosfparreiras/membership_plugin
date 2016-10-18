dd03907c466a10be1543f17cdddc60dab12e2415
marcosfparreiras <marcosfparreiras@gmail.com>
2016-10-18 00:56:11 -0200

    Refactor hotmembers handler for hotmart

--- app/lib/handlers/hotmembers-connect-hotmart.php
+++ app/lib/handlers/hotmembers-connect-hotmart.php
@@ -6,41 +6,51 @@ class HotmembersConnectHotmart {
   * This uses the $POST values to handle hotmart POST connection
   */
   public static function act($post) {
+    $opts = self::get_opts_from_post($post);
+    $memberships = self::get_memberships($post);
+    if(count($memberships) == 0) {
+      return "No membership area found for specification";
+    }
+
+    $opts = $post;
+    foreach ($memberships as $membership) {
+      self::handle_opts_for_membership($opts, $membership);
+    }
+  }
+
+  private static function handle_opts_for_membership($opts, $membership) {
+    $opts['role_slug'] = $membership->slug;
+    if (self::is_status_to_add($opts['status'])) {
+      $user_helper = new HotmembersConnectUserHelper($opts);
+      $user_helper->add_wp_user();
+      return 'User added';
+    }
+    elseif(self::is_status_to_remove($opts['status'])) {
+      $user_helper = new HotmembersConnectUserHelper($opts);
+      $user_helper->delete_wp_user();
+      return 'User deleted';
+    }
+  }
+
+  private static function get_opts_from_post($post) {
     $opts = array(
-      // 'hottok' => $post['hottok'],
       // 'transaction' => $post['transaction'],
+      'hottok' => $post['hottok'],
       'status' => $post['status'],
       'email' => $post['email'],
       'first_name' => $post['first_name'],
       'last_name' => $post['last_name'],
       'prod' => $post['prod']
     );
+    return $opts;
+  }
 
+  private static function get_memberships($opts) {
     $search_opts = array(
       'prod' => $opts['prod']
-      // 'hottok' => $hottok
+      // 'hottok' => $opts['hottok']
     );
-
-    $memberships = Membership_Area::where($search_opts);
-    if(count($memberships) == 0) {
-      echo "<br>no membership area found for specification";
-      return;
-    }
-
-    $opts = $post;
-    foreach ($memberships as $membership) {
-      $opts['role_slug'] = $membership->slug;
-      echo "<br>Membership_Area name: $membership->name";
-      if (self::is_status_to_add($opts['status'])) {
-        $user_helper = new HotmembersConnectUserHelper($opts);
-        $user_helper->add_wp_user();
-      }
-      elseif(self::is_status_to_remove($opts['status'])) {
-        echo "<br>status is to remove";
-        $user_helper = new HotmembersConnectUserHelper($opts);
-        $user_helper->delete_wp_user();
-      }
-    }
+    return Membership_Area::where($search_opts);
   }
 
   private static function is_status_to_add($status) {
