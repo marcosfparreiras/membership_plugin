58f7044a50b74c390e957f6fb74a64e13d9e3278
marcosfparreiras <marcosfparreiras@gmail.com>
2016-11-27 00:13:40 -0200

    Create lib for creating/removing wordpress users

--- app/lib/wordpress-user.php
+++ app/lib/wordpress-user.php
@@ -2,54 +2,44 @@
 namespace Hotmembers3;
 class Wordpress_User {
 
-  public function create_wp_user($user_email, $first_name, $last_name, $user_pass) {
-    $userdata = array(
-      'user_login' => $user_email,
-      'user_email' => $user_email,
-      'first_name' => $first_name,
-      'last_name' => $last_name,
-      'user_pass' => $user_pass
-    );
-    wp_insert_user( $userdata ) ;
+  public function __construct($user_email, $first_name, $last_name) {
+    $this->user_email = $user_email;
+    $this->first_name = $first_name;
+    $this->last_name = $last_name;
   }
 
+  public function add($role_slug) {
+    $user = self::get_or_create_user();
+    $user->add_role($role_slug);
+  }
 
-  // public function add_wp_user() {
-  //   echo "<br>status is to add";
-  //   $user = get_user_by('email', $this->email);
-  //   if ($user) {
-  //     echo "<br>user exists on create";
-  //     self::add_role_to_existing_user();
-  //   }
-  //   else {
-  //     echo "<br>user does not exist";
-  //     self::add_role_to_new_user();
-  //   }
+  public function remove($role_slug) {
+    $user = get_user_by('email', $this->user_email);
+    if ($user) {
+      $user->remove_role($role_slug);
+    }
+  }
 
-  // }
+  private function get_or_create_user() {
+    $user = get_user_by('email', $this->user_email);
+    if ($user) {
+      return $user;
+    }
+    else {
+      $user_id = self::create_wp_user();
+      return new \WP_USER($user_id);
+    }
+  }
 
-  // public function delete_wp_user() {
-  //   $user = get_user_by('email', $this->email);
-  //   if ($user) {
-  //     $this->user->remove_role($this->role_slug);
-  //   }
-  // }
-
-  // private function add_role_to_existing_user() {
-  //   $user = $this->user;
-  //   $user->add_role($this->role_slug);
-  // }
-
-  // private function add_role_to_new_user() {
-  //   $pass = wp_generate_password(8, false);
-  //   $userdata = array(
-  //     'user_pass' =>  $pass,
-  //     'user_login' => $this->email,
-  //     'user_email' => $this->email,
-  //     'first_name' => $this->first_name,
-  //     'last_name'  => $this->last_name,
-  //     'role' => $this->role_slug
-  //   );
-  //   wp_insert_user($userdata);
-  // }
+  private function create_wp_user() {
+    $userdata = array(
+      'user_login' => $this->user_email,
+      'user_email' => $this->user_email,
+      'first_name' => $this->first_name,
+      'last_name' => $this->last_name,
+      'user_pass' => wp_generate_password(8, false)
+    );
+    $user_id = wp_insert_user($userdata);
+    return new WP_USER($user_id);
+  }
 }
--- app/views/test.php
+++ app/views/test.php
@@ -1,8 +1,8 @@
 <?php
 namespace Hotmembers3;
 
-$wp_user = new Wordpress_User();
-$wp_user->create_wp_user('email@mail.test', 'FN', 'LN', 'mypass');
+$wp_user = new Wordpress_User('email002@mail.test', 'FN', 'LN');
+$wp_user->add('hm3_role_1476846444');
 // $date = \DateTime::createFromFormat('d/m/Y H:i', '25/10/2016 17:45');
 // echo $date->format('Y-m-d');
 ?>
